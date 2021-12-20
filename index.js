556f09fbbd2db38a142648bf354f48f9cd40a8f3
ShamsathMaleha <17203020@iubat.edu>
2021-12-21 00:07:25 +0600

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ucxei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
